6e5c5a8bc5e67000dcd284febb302d8a0e11a02e
Jason Farrow <jayfarrow007@gmail.com>
2023-12-14 15:43:44 -0500

    Updated main.py to include writing, reading, and updating to JSON data files. Deleted data.txt.

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import tkinter as tk
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
 GRAY = "#4a4a4a"
 WHITE = "#ffffff"
 
-#---------------------- Password Generator --------------------#
+
+# ---------------------- Password Generator --------------------#
 def password_generator():
     letters_symbols = [
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
         'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-        'Q', 'R','S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','0', '1', '2', '3', '4', '5', '6', '7', '8', '9','!',
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!',
         '#', '$', '%', '&', '(', ')', '*', '+'
     ]
     password_entry.delete(0, 'end')
@@ -19,9 +21,9 @@ def password_generator():
     number = 12
     for num in range(number):
         n = random.choice(letters_symbols)
-        password +=n
+        password += n
 
-    password_entry.insert(0,password)
+    password_entry.insert(0, password)
     pyperclip.copy(password)
 
 
@@ -31,6 +33,12 @@ def add_password():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
     website_entry.delete(0, 'end')
     password_entry.delete(0, 'end')
     if len(website) == 0:
@@ -40,11 +48,16 @@ def add_password():
     elif len(password) == 0:
         messagebox.showinfo(title="Input Password", message="You did not enter a password.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
-                                                  f"\nPassword: {password} \nIs it ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as file:
-                file.write(f"{website},{email},{password}\n")
+        try:
+            with open("data.json", "r") as file:
+                data = json.load(file)
+                data.update(new_data)
+        except FileNotFoundError:
+            with open("data.json", "w") as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            with open("data.json", "w") as file:
+                json.dump(data, file, indent=4)
 
 
 # --------------------- User Interface ------------------------#
@@ -65,13 +78,13 @@ center_y = int(screen_height / 2 - window_height / 2)
 window.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
 
 # Creating Canvas
-canvas = tk.Canvas(width= 200, height=200, bg=GRAY, highlightthickness=0)
+canvas = tk.Canvas(width=200, height=200, bg=GRAY, highlightthickness=0)
 logo_img = tk.PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
 # Creating Labels
-website_label = tk.Label(text="Website:", font=("Arial", 16), fg=WHITE, bg=GRAY, pady= 0)
+website_label = tk.Label(text="Website:", font=("Arial", 16), fg=WHITE, bg=GRAY, pady=0)
 website_label.grid(column=0, row=1)
 
 email_username_label = tk.Label(text="E-Mail/Username:", font=("Arial", 16), fg=WHITE, bg=GRAY, padx=5, pady=0)
@@ -87,7 +100,7 @@ website_entry.focus()
 
 email_entry = tk.Entry(window, width=61)
 email_entry.grid(column=1, row=2, columnspan=2)
-email_entry.insert(0,"jayfarrow30@gmail.com")
+email_entry.insert(0, "random@gmail.com")
 
 password_entry = tk.Entry(window, width=33, show="*")
 password_entry.grid(column=1, row=3)
